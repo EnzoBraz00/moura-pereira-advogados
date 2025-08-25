37cbdb299ba5b592afa88b1f900a88ec7a5d0492
Enzo Braz <enzo.braz@germinare.org.br>
2025-08-24 21:55:21 -0300

    adding media queries

--- src/components/header/HeaderComponent.jsx
+++ src/components/header/HeaderComponent.jsx
@@ -1,29 +1,123 @@
-import React from 'react'
-import './HeaderComponent.css'
-import logo from '../../assets/firm-logo.png'
-import { Link, useNavigate } from 'react-router-dom'
+import React, { useState } from "react";
+import "./HeaderComponent.css";
+import logo from "../../assets/firm-logo.png";
+import { Link, useNavigate } from "react-router-dom";
 
 const HeaderComponent = () => {
+  const [isSidebarOpen, setIsSidebarOpen] = useState(false);
   const navigate = useNavigate();
-  function handleLogoNavigation(){
-      navigate("/")
-  }
-  return (
-    <div className='header-container'>
-        <div className='header-top'>
-            <p>(11) 97448-1602</p>
-        </div>
-        <div className='header-content'>
-            <img onClick={() => handleLogoNavigation()} src={logo} alt="Moura Pereira Advogados Logo" className='header-logo' />
-            <div className='header-quick-access'>
-                <Link to="/" className='header-link'>HOME</Link>
-                <Link to="/working-area" className='header-link'>ÁREAS</Link>
-                <Link to="/blog" className='header-link'>POSTS</Link>
-                <Link to="/contact" className='header-link'>CONTATO</Link>
-            </div>
-        </div>
-    </div>
-  )
-}
 
-export default HeaderComponent
+  function handleLogoNavigation() {
+    navigate("/");
+    setIsSidebarOpen(false);
+  }
+
+  function handleAreasNavigation() {
+    navigate("/");
+    setIsSidebarOpen(false);
+    setTimeout(() => {
+      const workingAreasSection = document.querySelector(
+        ".working-areas-container"
+      );
+      if (workingAreasSection) {
+        const yOffset = -100;
+        const y =
+          workingAreasSection.getBoundingClientRect().top +
+          window.pageYOffset +
+          yOffset;
+        window.scrollTo({ top: y, behavior: "smooth" });
+      }
+    }, 100);
+  }
+
+  const toggleSidebar = () => {
+    setIsSidebarOpen(!isSidebarOpen);
+  };
+
+  const handleNavigation = (path) => {
+    navigate(path);
+    setIsSidebarOpen(false);
+  };
+
+  return (
+    <>
+      <div className="header-container">
+        <div className="header-top">
+          <p>(11) 97448-1602</p>
+        </div>
+        <div className="header-content">
+          <img
+            onClick={handleLogoNavigation}
+            src={logo}
+            alt="Moura Pereira Advogados Logo"
+            className="header-logo"
+          />
+          <div className="header-quick-access">
+            <Link to="/" className="header-link">
+              HOME
+            </Link>
+            <button
+              onClick={handleAreasNavigation}
+              className="header-link areas-button"
+            >
+              ÁREAS
+            </button>
+            <Link to="/blog" className="header-link">
+              POSTS
+            </Link>
+          </div>
+          <button
+            className="hamburger-menu"
+            onClick={toggleSidebar}
+            aria-label="Menu"
+          >
+            <span
+              className={`hamburger-line ${isSidebarOpen ? "active" : ""}`}
+            ></span>
+            <span
+              className={`hamburger-line ${isSidebarOpen ? "active" : ""}`}
+            ></span>
+            <span
+              className={`hamburger-line ${isSidebarOpen ? "active" : ""}`}
+            ></span>
+          </button>
+        </div>
+      </div>
+      <div className={`sidebar ${isSidebarOpen ? "sidebar-open" : ""}`}>
+        <div className="sidebar-content">
+          <button
+            className="sidebar-close"
+            onClick={() => setIsSidebarOpen(false)}
+          >
+            x
+          </button>
+          <nav className="sidebar-nav">
+            <button
+              onClick={() => handleNavigation("/")}
+              className="sidebar-link"
+            >
+              HOME
+            </button>
+            <button onClick={handleAreasNavigation} className="sidebar-link">
+              ÁREAS
+            </button>
+            <button
+              onClick={() => handleNavigation("/blog")}
+              className="sidebar-link"
+            >
+              POSTS
+            </button>
+          </nav>
+        </div>
+      </div>
+        {isSidebarOpen && (
+        <div
+          className="sidebar-overlay"
+          onClick={() => setIsSidebarOpen(false)}
+        ></div>
+      )}
+    </>
+  );
+};
+
+export default HeaderComponent;
--- src/pages/home/components/objectives/ObjectivesComponent.jsx
+++ src/pages/home/components/objectives/ObjectivesComponent.jsx
@@ -1,61 +1,80 @@
 import React from "react";
 import "./ObjectivesComponent.css";
 import missionIcon from "../../../../assets/Goal.svg";
-import visionImage from "../../../../assets/vision-image.jpg";
+import valuesImage from "../../../../assets/values-image.jpg";
+import missionImage from "../../../../assets/mission-image.jpg";
+import visionImage from "../../../../assets/vision-image.png";
 
 const ObjectivesComponent = () => {
   return (
     <div className="objectives-container">
-      <div id="objectives-box" className="mission-container">
-        <div id="title-container">
-          <div id="img-container">
+      <div className="objective-box mission-box">
+        <div className="objective-title">
+          <div className="objective-icon-container">
             <img
               src={missionIcon}
-              alt="Mission Icon"
-              className="mission-icon"
+              alt="Ícone Missão"
+              className="objective-icon"
             />
           </div>
-          <h2 id="title">Missão</h2>
+          <h2 className="objective-heading">Missão</h2>
         </div>
-        <p id="description">
-          Lorem ipsum dolor sit amet consectetur adipiscing elit quisque
-          faucibus ex sapien vitae pellentesque sem.
+        <p className="objective-description">
+          Atuar com excelência na defesa dos interesses de nossos clientes,
+          oferecendo soluções jurídicas estratégicas e seguras, sempre pautadas
+          pela ética.
         </p>
-        <img src={visionImage} alt="Vision" className="vision-image" id="image" />
+        <img
+          src={missionImage}
+          alt="Imagem representando missão"
+          className="objective-image"
+        />
       </div>
-      <div id="objectives-box" className="vision-container">
-        <div id="title-container">
-          <div id="img-container">
+
+      <div className="objective-box vision-box">
+        <div className="objective-title">
+          <div className="objective-icon-container">
             <img
               src={missionIcon}
-              alt="Mission Icon"
-              className="mission-icon"
+              alt="Ícone Visão"
+              className="objective-icon"
             />
           </div>
-          <h2 id="title">Visão</h2>
+          <h2 className="objective-heading">Visão</h2>
         </div>
-        <p id="description">
-          Lorem ipsum dolor sit amet consectetur adipiscing elit quisque
-          faucibus ex sapien vitae pellentesque sem.
+        <p className="objective-description">
+          Ser reconhecido como um dos escritórios de advocacia de maior
+          credibilidade e referência no cenário nacional, destacando-se pela
+          inovação e eficiência.
         </p>
-        <img src={visionImage} alt="Vision" className="vision-image" id="image" />
+        <img
+          src={visionImage}
+          alt="Imagem representando visão"
+          className="objective-image"
+        />
       </div>
-      <div id="objectives-box" className="values-container">
-        <div id="title-container">
-          <div id="img-container">
+
+      <div className="objective-box values-box">
+        <div className="objective-title">
+          <div className="objective-icon-container">
             <img
               src={missionIcon}
-              alt="Mission Icon"
-              className="mission-icon"
+              alt="Ícone Valores"
+              className="objective-icon"
             />
           </div>
-          <h2 id="title">Valores</h2>
+          <h2 className="objective-heading">Valores</h2>
         </div>
-        <p id="description">
-          Lorem ipsum dolor sit amet consectetur adipiscing elit quisque
-          faucibus ex sapien vitae pellentesque sem.
+        <p className="objective-description">
+          Ética, responsabilidade social, dedicação, respeito às pessoas,
+          compromisso com a verdade e busca constante pela excelência em cada
+          serviço prestado.
         </p>
-        <img src={visionImage} alt="Vision" className="vision-image" id="image" />
+        <img
+          src={valuesImage}
+          alt="Imagem representando valores"
+          className="objective-image"
+        />
       </div>
     </div>
   );
